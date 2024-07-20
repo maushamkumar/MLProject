d60b9e198497e252f7d021d0c98943ee683cc6e4
maushamkumar <144700160+maushamkumar@users.noreply.github.com>
2024-07-20 11:03:06 +0530

    Update setup.py with corrections and improvements

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_requirements(file_path: str) -> List[str]:
 setup(
     name='mlproject',
     version='0.0.1',
-    author='Mausham',
+    author='Mausham kumar',
     author_email='maushamkumarr26@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
