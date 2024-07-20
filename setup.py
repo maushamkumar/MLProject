3bbb94b49015138b0b9af6a4c84686c032cd21b9
maushamkumar <144700160+maushamkumar@users.noreply.github.com>
2024-07-20 11:00:37 +0530

    update README.md

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+# This will automatically find out all the packages that are available 
+# in the entire ml application in the directory that we have actually created 
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function will return the list of requirements 
+    '''
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Mausham',
+    author_email='maushamkumarr26@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
