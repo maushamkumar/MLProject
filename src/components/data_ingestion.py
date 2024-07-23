aa797394e61485b7d847b0090aa73b29db2cadc2
maushamkumar <144700160+maushamkumar@users.noreply.github.com>
2024-07-23 11:58:50 +0530

    Modify data_ingestion file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,71 +1,27 @@
-# import os 
-# import sys 
-# from src.exception import CustomException
-# from src.logger import logging
-# import pandas as pd 
-
-# from sklearn.model_selection import train_test_split
-# from dataclasses import dataclass
-
-
-# @dataclass
-# class DataIngestionConfig:
-#     train_data_path: str=os.path.join('artifacts', 'train.csv')  # all the output will basically be stored inside the artifact folder and another is my file name 
-#     test_data_path: str=os.path.join('artifacts', 'test.csv')
-#     raw_data_path: str=os.path.join('artifacts', 'data.csv') # These are the input that i'm giving to my data ingestion component and now data ingestion component know where to save to the train path test path and data path
-#     # This is input I am giving latter on my data ingestion will save the train dot csv file in this particular path 
-    
-# class DataIngestion:
-#     def __init__(self):
-#         self.ingestion_config =DataIngestionConfig() # This ingestion underscore config will consist of this three values becasue this is the input that i nearly need to initialize 
-    
-#     def initiate_data_ingestion(self):
-#         logging.info('Entered the data Ingestion method or component')
-#         try:
-#             df = pd.read_csv('notebook/data/stud.csv')
-#             logging.info("Read the dataset as dataframe")
-            
-#             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
-            
-#             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
-            
-#             logging.info("Train test split initialted")
-#             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
-            
-#             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-#             train_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
-            
-#             logging.info("Ingestion of the data is completed")
-            
-#             return (
-#                 self.ingestion_config.train_data_path, 
-#                 self.ingestion_config.test_data_path
-#             )
-#         except Exception as e:
-#             raise CustomException(e, sys)
-        
-# if __name__ == "__main__":
-#     obj = DataIngestion()
-#     obj.initiate_data_ingestion()
-    
-    
 import os 
 import sys 
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd 
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    train_data_path: str = os.path.join('artifacts', 'train.csv')  # all the output will basically be stored inside the artifact folder and another is my file name 
     test_data_path: str = os.path.join('artifacts', 'test.csv')
-    raw_data_path: str = os.path.join('artifacts', 'data.csv')
-
+    raw_data_path: str = os.path.join('artifacts', 'data.csv') # These are the input that i'm giving to my data ingestion component and now data ingestion component know where to save to the train path test path and data path
+    # This is input I am giving latter on my data ingestion will save the train dot csv file in this particular path 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config = DataIngestionConfig()
+        self.ingestion_config = DataIngestionConfig()  # This ingestion underscore config will consist of this three values becasue this is the input that i nearly need to initialize 
     
     def initiate_data_ingestion(self):
         logging.info('Entered the data Ingestion method or component')
@@ -93,5 +49,11 @@ class DataIngestion:
             raise CustomException(e, sys)
         
 if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
